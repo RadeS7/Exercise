9a69be6096f25aa9c7d8ac8c8943ca257499a11d
RadeS7 <rades7@gmail.com>
2021-12-06 21:05:13 +0100

    work from home

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,52 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("this is test exercise for github");
+        System.out.println(numberToWords(15));
+    }
+
+    public static String numberToWords(int number) {
+        if (number < 0) {
+            System.out.println("Invalid value");
+        }
+        String word = null;
+        while (number > 0) {
+            int divider = number % 10; // uzima zadnju cifru
+            switch (divider) {
+                case 1:
+                    word = "One";
+                    break;
+                case 2:
+                    word = "Two";
+                    break;
+                case 3:
+                    word = "Three";
+                    break;
+                case 4:
+                    word = "Four";
+                    break;
+                case 5:
+                    word = "Five";
+                    break;
+                case 6:
+                    word = "Six";
+                    break;
+                case 7:
+                    word = "Seven";
+                    break;
+                case 8:
+                    word = "Eight";
+                    break;
+                case 9:
+                    word = "Nine";
+                    break;
+                case 0:
+                    word = "Zero";
+                    continue;
+                number = number / 10 ;
+            }return word;
+
+
+
+        } return " + ";
     }
 }
